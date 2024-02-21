5fef77b5ce101a8b39dd1de9fd2084d395bbe1c8
João Augusto <joaoaugusto311016@gmail.com>
2024-02-21 17:57:25 -0300

    fourth commit

--- node/src/shared/infrastructure/env-config/env-config.service.ts
+++ node/src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfig } from './envi-config.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  constructor(private configService: ConfigService) {}
+  getAppPort(): number {
+    return Number(value:this.configService.get<number>(propertyPath: 'PORT'));
+  }
+  getNodeEnvi(): string {
+    return this.configService.get<string>(propertyPath: 'NODE_ENV');
+  }
+}
